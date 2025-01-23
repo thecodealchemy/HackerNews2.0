a687ff9140bcec176bfd73398fe6a05886b0d1be
JoyBoy <adityasingh1235467890@gmail.com>
2025-01-23 15:10:52 +0530

    nit

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ function App() {
       const endpoint = `${type}stories`;
       const response = await fetch(`https://hacker-news.firebaseio.com/v0/${endpoint}.json`);
       const storyIds = await response.json();
-      const promises = storyIds.slice(0, 30).map(id =>
+      const promises = storyIds.slice(0, 30).map((id: any) =>
         fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then(res => res.json())
       );
       const fetchedStories = await Promise.all(promises);
